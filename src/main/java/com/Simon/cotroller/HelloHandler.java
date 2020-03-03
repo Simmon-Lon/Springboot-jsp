c58d5ce96896ef0e1fc144207a6da5d6bb918e81
Simon-Lon <11496543250@qq.com>
2020-03-03 13:19:33 +0800

    branch test

--- src/main/java/com/Simon/cotroller/HelloHandler.java
+++ src/main/java/com/Simon/cotroller/HelloHandler.java
@@ -4,10 +4,7 @@ import com.Simon.entity.Student;
 import com.Simon.repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
